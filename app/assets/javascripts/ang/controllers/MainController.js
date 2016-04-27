84dbef554024d0f24db899f8b78e69e987bb2e93
pajamaw <pjpeterww@gmail.com>
2016-04-27 14:29:32 -0400

    the current user  can only see the  markers on their map, also added markers via click events

--- app/assets/javascripts/ang/controllers/MainController.js
+++ app/assets/javascripts/ang/controllers/MainController.js
@@ -7,9 +7,31 @@ function MainController($scope, List, $location, $state, Auth, uiGmapGoogleMapAp
      // areaZoom     = 3;
 
   uiGmapGoogleMapApi.then(function(maps) {
-    $scope.map     = { center: { latitude: 44, longitude: 100 }, zoom: 3 };
-    $scope.options = { scrollwheel: false };
+    $scope.map     = { center: { latitude: 40.783435, longitude: -73.966249 }, zoom: 12,
+    markers: [],
+    events: { "click" : 
+      function (map, eventname, originalEventArgs) { 
+        //alert(maps.latLng)
+        var e = originalEventArgs[0];
+        var lat = e.latLng.lat(),lon = e.latLng.lng();
+        var marker = {
+          id: Date.now(),
+          coords:{
+            latitude: lat,
+            longitude: lon
+          }
+        };
+        $scope.map.markers.push(marker);
+        console.log('"lat:' + marker.coords.latitude + ', lon:' + marker.coords.longitude + '"');
+        $scope.$apply();
+      }
+    },
+
+    options: { scrollwheel: false }
     //debugger;
+    //$scope.getpos = function(event) {
+    //alert(event.latLng);
+    };
   });
 
   Auth.currentUser()
@@ -24,6 +46,8 @@ function MainController($scope, List, $location, $state, Auth, uiGmapGoogleMapAp
   };
 
   $scope.changeTodo = function (i) {
+     console.log(i); 
+
     $scope.currentShow = i;
   };
   //ctrl.list = new List();
