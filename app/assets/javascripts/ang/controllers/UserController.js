8d59a148935873cde7e670043dec4afaabe7b951
pajamaw <pjpeterww@gmail.com>
2016-05-07 15:25:15 -0600

    Deleting comments in MainController, standardizing initial angular module call throughout controllers and services

--- app/assets/javascripts/ang/controllers/UserController.js
+++ app/assets/javascripts/ang/controllers/UserController.js
@@ -1,8 +1,6 @@
+app.controller('UserController', UserController)
+
 function UserController(user) {
 
   this.data = user.data;
 }
-
-angular
-  .module('app')
-  .controller('UserController', UserController);
